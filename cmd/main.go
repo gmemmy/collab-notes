f4357b9f329aff280ce36fc0e5500adb912c0661
Emmanuel Atawodi <gmemmy22@gmail.com>
2025-05-18 12:54:07 +0100

    chore: create project scaffold

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+
+	"collab-notes/internal/handlers"
+	"collab-notes/internal/middleware"
+)
+
